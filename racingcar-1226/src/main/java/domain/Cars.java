d2345a447e67466288a570f51113fea945f1165a
haechanmoon <timmymoon@naver.com>
2025-12-26 17:25:00 +0900

    feat(Cars) : 우승자 구하기

---
+++ racingcar-1226/src/main/java/domain/Cars.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    private List<String> getWinners() {
+        int count = 0;
+        List<String> names = new ArrayList<>();
+        count = getMaxPosition(count);
+        getWinner(count, names);
+        return names;
+    }
+
+    private void getWinner(int count, List<String> names) {
+        for (Car car : cars) {
+            if (count == car.getPosition()) {
+                names.add(car.getName());
+            }
+        }
+    }
+
+    private int getMaxPosition(int count) {
+        for (Car car : cars) {
+            if (count <= car.getPosition()) {
+                count = car.getPosition();
+            }
+        }
+        return count;
+    }
+
+    public String getWinnerList() {
+        return String.join(", ", getWinners());
+    }
+
+}
