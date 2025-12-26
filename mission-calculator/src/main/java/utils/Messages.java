bf647c8eb67de9c8a8312646d8e2c18f591b2625
haechanmoon <timmymoon@naver.com>
2025-12-26 09:15:06 +0900

    feat : 문자열 입력 요구 출력

---
+++ mission-calculator/src/main/java/utils/Messages.java
@@ -0,0 +1,5 @@
+package utils;
+
+public class Messages {
+    public static final String REQUEST_INPUT = "덧셈할 문자열을 입력해 주세요.";
+}
---
+++ mission-calculator/src/main/java/view/OutputView.java
@@ -0,0 +1,9 @@
+package view;
+
+import utils.Messages;
+
+public class OutputView {
+    public static void requestInput() {
+        System.out.println(Messages.REQUEST_INPUT);
+    }
+}
