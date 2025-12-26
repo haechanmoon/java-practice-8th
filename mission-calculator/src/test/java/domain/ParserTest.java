036755a525571ffbc03ecb55583b19cd8baa36b7
haechanmoon <timmymoon@naver.com>
2025-12-26 12:27:36 +0900

    test(ParserTest) : (예외) 숫자가 아닌 수가 들어갔을 때

---
+++ mission-calculator/src/test/java/domain/ParserTest.java
@@ -0,0 +1,23 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import utils.Messages;
+
+class ParserTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,f,5", "1f,1,2", "-1,2", "3p,1"})
+    @DisplayName("숫자가 아닌 수가 들어갔을 때")
+    void 숫자인지_검증(String input) {
+        Parser parser = new Parser(input);
+
+        assertThatThrownBy(parser::parseInput)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Messages.ERROR_IS_NOT_DIGIT);
+        
+    }
+}
