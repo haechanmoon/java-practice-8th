e232b998367e88dfbb493b89542964af45db3ace
haechanmoon <timmymoon@naver.com>
2025-12-27 18:51:18 +0900

    feat(Cars) : 자동차들 1회 전진

--- racingcar-1226/src/main/java/domain/Cars.java
+++ racingcar-1226/src/main/java/domain/Cars.java
@@ -1,7 +1,9 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Cars {
     private final List<Car> cars;
@@ -37,6 +39,32 @@ public class Cars {
 
     public String writeWinners() {
         return String.join(", ", getWinners());
+    }
+
+    private void goCars() {
+        RandomGenerator generator = new RandomGenerator();
+        for (Car car : cars) {
+            car.go(generator.generateRandom());
+        }
+    }
+
+    public Map<Car, Integer> runTurns(int turns) {
+        Map<Car, Integer> position = new HashMap<>();
+        for (int i = 0; i < turns; i++) {
+            goCars();
+            for (Car car : cars) {
+                position.put(car, car.getPosition());
+            }
+        }
+        return position;
+    }
+
+    public String printResult() {
+        StringBuilder resultBoard = new StringBuilder();
+        for (Car car : cars) {
+            resultBoard.append(car.currentPosition()).append("\n");
+        }
+        return resultBoard.toString();
     }
 
 }
