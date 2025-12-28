d8eb7f1945c9f6943248d5e214ed7613061084a7
haechanmoon <timmymoon@naver.com>
2025-12-28 22:47:29 +0900

    feat(Rank) : 당첨 통계 Rank에게 일을 시킨다.

--- lotto-1228/src/main/java/domain/Rank.java
+++ lotto-1228/src/main/java/domain/Rank.java
@@ -61,5 +61,12 @@ public enum Rank {
     public int getPrizeMoney() {
         return prizeMoney;
     }
-    
+
+    public String getString() {
+        if (this == SECOND) {
+            return String.format(("%d개 일치, 보너스 볼 일치(%,d원)"), matchCount, prizeMoney);
+        }
+        return String.format(("%d개 일치, (%,d원)"), matchCount, prizeMoney);
+    }
+
 }
