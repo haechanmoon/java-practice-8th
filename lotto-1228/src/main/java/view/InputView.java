92e8776bc75d29be3a82fbbc3a1863eaaf635ec6
haechanmoon <timmymoon@naver.com>
2025-12-28 19:52:33 +0900

    feat(InputView) : (검증) 빈 값은 아닌지

--- lotto-1228/src/main/java/view/InputView.java
+++ lotto-1228/src/main/java/view/InputView.java
@@ -4,17 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 import utils.Validator;
 
 public class InputView {
-    public static String inputMoney() {
+    public static String inputRead() {
         String input = Console.readLine();
         Validator.validateIsBlank(input);
         return input;
     }
 
-    public static String inputNumbers() {
-        return Console.readLine();
-    }
-
-    public static String inputBonusNumber() {
-        return Console.readLine();
-    }
 }
