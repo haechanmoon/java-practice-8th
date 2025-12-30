06e3616b467f691e9c43951d0cea343253968a2a
haechanmoon <timmymoon@naver.com>
2025-12-30 16:08:44 +0900

    feat(WinningNumbers) : 당첨번호를 로또번호와 비교

---
+++ lotto-1230/src/main/java/domain/WinningNumbers.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.util.List;
+
+public class WinningNumbers {
+
+    private final List<Integer> winNums;
+
+    public WinningNumbers(List<Integer> winNums) {
+        this.winNums = winNums;
+    }
+
+    public int matchLotto(Lotto lotto) {
+        return lotto.matchWinningNumbers(winNums);
+    }//이게 필요한건가? 일단 보류
+
+}
