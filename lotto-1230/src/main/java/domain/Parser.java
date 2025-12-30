e8cc3314fbd40d293ed260b44b81de4b0f6bb99d
haechanmoon <timmymoon@naver.com>
2025-12-30 16:18:38 +0900

    feat(Parser) : 입력된 문자를 List<Integer> 로 변환

--- lotto-1230/src/main/java/domain/Parser.java
+++ lotto-1230/src/main/java/domain/Parser.java
@@ -1,9 +1,21 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Parser {
 
     public static int parseMoney(String input) {
         return Integer.parseInt(input);
     }
 
+    public static List<Integer> parseNumbers(String input) {
+        List<String> numberStr = new ArrayList<>(Arrays.asList(input.split(",")));
+        List<Integer> numberInt = new ArrayList<>();
+        for (String val : numberStr) {
+            numberInt.add(Integer.parseInt(val));
+        }
+        return numberInt;
+    }
 }
