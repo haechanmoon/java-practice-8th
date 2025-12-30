ba12774e15d51e633f28f548d6de1433cff72ef6
haechanmoon <timmymoon@naver.com>
2025-12-30 16:02:43 +0900

    feat(Lottos) : 로또들의 번호를 문자열로 반환

---
+++ lotto-1230/src/main/java/domain/Lottos.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.util.List;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public String lottosToString() {
+        StringBuilder lottoBoard = new StringBuilder();
+        for (Lotto lotto : lottos) {
+            lottoBoard.append(lotto.toString()).append("\n");
+        }
+        return lottoBoard.toString();
+    }
+
+}
