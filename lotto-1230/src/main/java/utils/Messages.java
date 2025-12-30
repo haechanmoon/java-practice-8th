eb08bf80b92090b80d98fd18fb6498f72a840403
haechanmoon <timmymoon@naver.com>
2025-12-30 17:37:12 +0900

    feat(InputView) : (검증) 입력된 문자가 빈값은 아닌지 -> (InputView에 위임)

---
+++ lotto-1230/src/main/java/utils/Messages.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Messages {
+    public static final String REQUEST_MONEY = "구입금액을 입력해 주세요.";
+    public static final String ERROR_IS_BLANK = "[ERROR] 값을 입력해 주세요.";
+}
