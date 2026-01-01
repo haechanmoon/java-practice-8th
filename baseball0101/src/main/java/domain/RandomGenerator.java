ca97d4f66cbd71131a74d8eacae7d29ebe70ba67
haechanmoon <timmymoon@naver.com>
2026-01-01 12:40:02 +0900

    feat(RandomGenerator) : 겹치지 않는 int 랜덤값 생성

---
+++ baseball0101/src/main/java/domain/RandomGenerator.java
@@ -0,0 +1,10 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomGenerator implements NumberGenerator{
+    @Override
+    public int generate(){
+        return Randoms.pickNumberInRange(1, 9);
+    }
+}
