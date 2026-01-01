d8812de9ba106347eb2dab1d83e8889714ba85a4
haechanmoon <timmymoon@naver.com>
2026-01-01 13:39:52 +0900

    feat(Parser) : (검증) 숫자가 겹치지 않는지

--- baseball0101/src/main/java/utils/Messages.java
+++ baseball0101/src/main/java/utils/Messages.java
@@ -1,12 +1,10 @@
 package utils;
 
 public class Messages {
-
-
     private static final String ERROR_PREFIX = "[ERROR] ";
 
     public static final String ERROR_IS_NOT_DIGIT = ERROR_PREFIX+"숫자를 입력해주시기 바랍니다.";
     public static final String ERROR_IS_NOT_SIZE = ERROR_PREFIX + "숫자의 자릿수를 확인 바랍니다.";
-
+    public static final String ERROR_NUMBER_DUPLICATED = ERROR_PREFIX + "숫자가 중복되었습니다.";
 
 }
--- baseball0101/src/main/java/utils/Validator.java
+++ baseball0101/src/main/java/utils/Validator.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.HashSet;
+
 public class Validator {
     public static void validateIsDigit(String input) {
         if(!input.matches("\\d+")){
@@ -8,6 +10,13 @@ public class Validator {
     }
 
     public static void ValidateIsDuplicated(String input) {
+        HashSet<Character> validator = new HashSet<>();
+        for(int i = 0 ; i<input.length();i++){
+            validator.add(input.charAt(i));
+        }
+        if(validator.size()!=input.length()){
+            throw new IllegalArgumentException(Messages.ERROR_NUMBER_DUPLICATED);
+        }
 
     }
 
