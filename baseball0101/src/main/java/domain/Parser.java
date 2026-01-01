42b0ee7f11dfec0a938434c2c792b3f7ce5ce12b
haechanmoon <timmymoon@naver.com>
2026-01-01 13:29:23 +0900

    feat(Parser) : 받은 문자열을 숫자로 변환

--- baseball0101/src/main/java/domain/Parser.java
+++ baseball0101/src/main/java/domain/Parser.java
@@ -1,4 +1,20 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import utils.Validator;
+
 public class Parser {
+
+
+    public static List<Integer> convertString(String input){
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0 ; i<input.length();i++){
+            int num = input.charAt(i)-'0';
+            numbers.add(num);
+        }
+        return numbers;
+    }
+
+
 }
