73d5bd43d59f66c70421dc3c90c7a9c8a45e5b73
haechanmoon <timmymoon@naver.com>
2026-01-02 14:42:56 +0900

    feat(Month) : 받은 값이 몇월인지 반환

--- oncall0102/src/main/java/domain/Month.java
+++ oncall0102/src/main/java/domain/Month.java
@@ -1,4 +1,33 @@
 package domain;
 
 public enum Month {
+    JAN(1, 31),
+    FEB(2, 28),
+    MARCH(3, 31),
+    APRIL(4, 30),
+    MAY(5, 31),
+    JUNE(6, 30),
+    JULY(7, 31),
+    AUG(8, 31),
+    SEP(9, 30),
+    OCT(10, 31),
+    NOV(11, 30),
+    DEC(12, 31);
+
+    private final int num;
+    public final int dateCount;
+
+    Month(int num, int dateCount) {
+        this.num = num;
+        this.dateCount = dateCount;
+    }
+
+    public Month valueOf(int num) {
+        for (Month month : Month.values()) {
+            if (month.num == num) {
+                return month;
+            }
+        }
+        return DEC;
+    }
 }
