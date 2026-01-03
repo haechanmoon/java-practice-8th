439c2b5ee02549d82b06e68012e33df0fb94e510
haechanmoon <timmymoon@naver.com>
2026-01-03 13:55:39 +0900

    feat(Menu) : 자신의 메뉴를 반환

--- christmas0103/src/main/java/domain/Menu.java
+++ christmas0103/src/main/java/domain/Menu.java
@@ -1,4 +1,44 @@
 package domain;
 
+import utils.Messages;
+
 public enum Menu {
+
+    SOUP("양송이수프", 6000, MenuType.EPI),
+    TAPAS("타파스", 5500, MenuType.EPI),
+    SALAD("시저샐러드", 8000, MenuType.EPI),
+    STAKE("티본스테이크", 55000, MenuType.MAIN),
+    LIP("바베큐립", 54000, MenuType.MAIN),
+    SEAPASTA("해산물파스타", 35000, MenuType.MAIN),
+    CHRPASTA("크리스마스파스타", 25000, MenuType.MAIN),
+    CHOCOCAKE("초코케이크", 15000, MenuType.DESSERT),
+    ICECREAM("아이스크림", 5000, MenuType.DESSERT),
+    ZEROCOKE("제로콜라", 3000, MenuType.DRINK),
+    REDWINE("레드와인", 60000, MenuType.DRINK),
+    CHAMPAIN("샴페인", 25000, MenuType.DRINK);
+
+    private final String name;
+    private final int cost;
+    private final MenuType menuType;
+
+    Menu(String name, int cost, MenuType menuType) {
+        this.name = name;
+        this.cost = cost;
+        this.menuType = menuType;
+    }
+
+    public static Menu converMenu(String input) {
+        for (Menu menu : Menu.values()) {
+            if (menu.name.equals(input)) {
+                return menu;
+            }
+        }
+        throw new IllegalArgumentException(Messages.ERROR_NO_MENU);
+    }
+
+
+}
+
+enum MenuType {
+    EPI, MAIN, DESSERT, DRINK;
 }
--- christmas0103/src/main/java/utils/Messages.java
+++ christmas0103/src/main/java/utils/Messages.java
@@ -1,4 +1,6 @@
 package utils;
 
 public class Messages {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String ERROR_NO_MENU = ERROR_PREFIX + "없는메뉴입니다.";
 }
