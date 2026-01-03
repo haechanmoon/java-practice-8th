73167736787027d5b60ffb28910c5b09d393f478
haechanmoon <timmymoon@naver.com>
2026-01-03 16:56:52 +0900

    feat(Order) : 디저트가 몇개 있는지 반환, 메인이 몇개 있는지 반환

--- christmas0103/src/main/java/domain/Order.java
+++ christmas0103/src/main/java/domain/Order.java
@@ -1,4 +1,22 @@
 package domain;
 
+import java.util.Map;
+
 public class Order {
+
+    private final Map<Menu, Integer> items;
+
+    public Order(Map<Menu, Integer> items) {
+        this.items = items;
+    }
+
+    public int menuCount(MenuType menuType) {
+        int count = 0;
+        for (Menu menu : Menu.values()) {
+            if (menu.getMenuType() == menuType) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
