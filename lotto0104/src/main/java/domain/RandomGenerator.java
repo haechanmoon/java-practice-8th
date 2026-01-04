77150b6f3ed796daffb8553335ca29475afd31a2
haechanmoon <timmymoon@naver.com>
2026-01-04 21:33:22 +0900

    feat(Randomgenerator) :  6개의 무작위 숫자 추출

---
+++ lotto0104/src/main/java/domain/RandomGenerator.java
@@ -0,0 +1,16 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomGenerator implements NumberGenerator {
+
+    private static final int START = 1;
+    private static final int END = 45;
+    private static final int COUNT = 6;
+
+    @Override
+    public List<Integer> generate(){
+        return Randoms.pickUniqueNumbersInRange(START,END,COUNT);
+    }
+}
