9b5cf00e21430e16df2e7598a91e2fc15edf41b6
haechanmoon <timmymoon@naver.com>
2026-01-04 21:14:08 +0900

    feat(WinningNumbers) : 검증 완료

--- lotto0104/src/main/java/domain/WinningNumbers.java
+++ lotto0104/src/main/java/domain/WinningNumbers.java
@@ -1,4 +1,22 @@
 package domain;
 
+import java.util.List;
+import java.util.Map;
+import utils.Validator;
+
 public class WinningNumbers {
+
+    private final List<Integer> winNums;
+
+    public WinningNumbers(List<Integer> winNums) {
+        Validator.validateRange(winNums);
+        Validator.validateDuplicated(winNums);
+        Validator.validateSize(winNums);
+        this.winNums = winNums;
+    }
+
+    public Map<Rank, Integer> calculate(Lottos lottos, int bonus){
+
+    }
+
 }
