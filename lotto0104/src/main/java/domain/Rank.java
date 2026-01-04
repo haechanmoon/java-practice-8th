0871c019d49a345e2d746fa0c81042987bfa16ed
haechanmoon <timmymoon@naver.com>
2026-01-04 21:28:06 +0900

    feat(Rank) : 자신의 랭크 반환

--- lotto0104/src/main/java/domain/Rank.java
+++ lotto0104/src/main/java/domain/Rank.java
@@ -1,4 +1,37 @@
 package domain;
 
+import utils.Messages;
+
 public enum Rank {
+
+    MISS(0,0),
+    FIFTH(3,5000),
+    FOUTH(4,50000),
+    THIRD(5,150000),
+    SECOND(5,3000000),
+    FIRST(6,2000000000)
+    ;
+
+    private final int matchCount;
+    private final int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public static Rank valueOf(int matchCount, boolean bonus){
+        if(matchCount<3){
+            return MISS;
+        }
+        if(matchCount==5&&bonus){
+            return SECOND;
+        }
+        for(Rank rank : Rank.values()){
+            if(rank.matchCount==matchCount){
+                return rank;
+            }
+        }
+        throw new IllegalArgumentException(Messages.ERROR_RANK);
+    }
 }
--- lotto0104/src/main/java/utils/Messages.java
+++ lotto0104/src/main/java/utils/Messages.java
@@ -4,4 +4,5 @@ public class Messages {
     public static final String ERROR_SIZE = "발행된 로또 번호 개수를 확인해주세요.";
     public static final String ERROR_DUPLICATED = "번호가 중복되었습니다.";
     public static final String ERROR_RANGE_OUT = "번호가 범위를 확인해주세요.";
+    public static final String ERROR_RANK = "없는 등수 입니다.";
 }
