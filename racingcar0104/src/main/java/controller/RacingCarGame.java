5e8a6359de1ea3646f220a8f7bc928f42a5835b3
haechanmoon <timmymoon@naver.com>
2026-01-04 16:29:38 +0900

    feat : 초기 구조 설정

---
+++ racingcar0104/src/main/java/controller/RacingCarGame.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class RacingCarGame {
+    public void start() {
+
+    }
+}
---
+++ racingcar0104/src/main/java/racingcar/Application.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import controller.RacingCarGame;
+
+public class Application {
+    public static void main(String[] args) {
+        RacingCarGame game = new RacingCarGame();
+        game.start();
+    }
+}
