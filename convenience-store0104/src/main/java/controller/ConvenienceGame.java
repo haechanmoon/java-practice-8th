13fd9469ea7efe4ad8ad16a70ba103a7240bf56c
haechanmoon <timmymoon@naver.com>
2026-01-04 16:24:15 +0900

    아 진짜 깨달았다. 이거 구조를 보니까 최종코테문제랑 똑같네. 이거를 내가 지금 짜고 있을 필요가 없었던거야. 최종 코테의 본질을 알아버렸다. 물론 로직을 짜는게 굉장히 중요하긴 하겠지만. 2,3주차 문제들을 잘 짤 수 있는 능력이 굉장히 중요해. 나는 이제 패스한다. 이전기수 문제들.

---
+++ convenience-store0104/src/main/java/controller/ConvenienceGame.java
@@ -0,0 +1,31 @@
+package controller;
+
+import view.InputView;
+import view.OutputView;
+
+public class ConvenienceGame {
+    public void start() {
+//        String s1 =InputView.readInput();
+//        String s2 = InputView.readInput();
+//        String s3 = InputView.readInput();
+//
+//        if(s1.equals("[물-1]")&&s2.equals("N")&&s3.equals("N")){
+//        OutputView.printQ1();
+//        }
+//
+//        if(s1.equals("[비타민워터-3],[물-2],[정식도시락-2]")&&s2.equals("N")&&s3.equals("N")){
+//            OutputView.printQ2();
+//        }
+//
+//        if(s1.equals("[감자칩-2]")&&s2.equals("N")&&s3.equals("N")){
+//            OutputView.printQ3();
+//        }
+//
+//        if(s1.equals("[컵라면-12]")){
+//            OutputView.printQ4();
+//        }
+
+
+
+    }
+}
---
+++ convenience-store0104/src/main/java/store/Application.java
@@ -0,0 +1,10 @@
+package store;
+
+import controller.ConvenienceGame;
+
+public class Application {
+    public static void main(String[] args) {
+        ConvenienceGame game = new ConvenienceGame();
+        game.start();
+    }
+}
