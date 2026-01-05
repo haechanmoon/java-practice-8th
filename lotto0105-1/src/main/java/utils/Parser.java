e5ccb681f0c50ec61011470ad4a2e2953be7fcc5
haechanmoon <timmymoon@naver.com>
2026-01-05 15:43:28 +0900

    feat(Parser) : 당첨 번호를 받고 List<Integer> 로 반환 (검증) 모든 번호가 숫자형태인가?

--- lotto0105-1/src/main/java/utils/Parser.java
+++ lotto0105-1/src/main/java/utils/Parser.java
@@ -1,10 +1,22 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parser {
 
     public static int parseMoney(String input){
         Validator.validateIsDigit(input);
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> parseNumbers(String input){
+        List<Integer> numbers = new ArrayList<>();
+        for(String val : input.split(",")){
+            Validator.validateIsDigit(val);
+            numbers.add(Integer.parseInt(val));
+        }
+        return numbers;
     }
 
 
