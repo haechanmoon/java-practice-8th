a8997c5aa2ed550ca1ef80dc957dd377286a5769
haechanmoon <timmymoon@naver.com>
2026-01-05 16:06:22 +0900

    feat(Statistics) : 수익률 계산 반환

--- lotto0105-1/src/main/java/domain/Statistics.java
+++ lotto0105-1/src/main/java/domain/Statistics.java
@@ -1,4 +1,21 @@
 package domain;
 
+import java.util.Map;
+
 public class Statistics {
+
+    private final Map<Rank, Integer> result;
+
+    public Statistics(Map<Rank, Integer> result) {
+        this.result = result;
+    }
+
+    public double ratio(int money){
+        double total = 0;
+        for(Rank rank : Rank.values()){
+            total += result.getOrDefault(rank, 0)*rank.getPrize();
+        }
+        return total/money;
+    }
+
 }
