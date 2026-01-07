270f9ef4c38d7b9814983cfb1c8cdb96e7334c36
haechanmoon <timmymoon@naver.com>
2026-01-07 20:46:58 +0900

    feat(Rank) : 1등~꽝을 가진 랭크를 반환

--- vendingmachine0107/src/main/java/domain/Rank.java
+++ vendingmachine0107/src/main/java/domain/Rank.java
@@ -1,4 +1,29 @@
 package domain;
 
 public enum Rank {
+
+    MISS(69,0,"꽝"),
+    THIRD(89,1000,"3등"),
+    SECOND(99,5000,"2등"),
+    FIRST(100,50000,"1등")
+    ;
+
+    private final int matchNum;
+    private final int prize;
+    private final String name;
+
+    Rank(int matchNum, int prize, String name) {
+        this.matchNum = matchNum;
+        this.prize = prize;
+        this.name = name;
+    }
+
+    public Rank matchRank(int num){
+        for(Rank rank : Rank.values()){
+           if(num<matchNum){
+               return rank;
+           }
+        }
+        return FIRST;
+    }
 }
