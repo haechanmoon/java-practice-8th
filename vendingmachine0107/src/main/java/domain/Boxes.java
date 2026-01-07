14a0f6446c5dfc12588d6e862dd2015b2a516fc3
haechanmoon <timmymoon@naver.com>
2026-01-07 21:20:11 +0900

    feat(Boxes) : Map을 이용하여 각 랭크당 박스의 개수반환

--- vendingmachine0107/src/main/java/domain/Boxes.java
+++ vendingmachine0107/src/main/java/domain/Boxes.java
@@ -1,6 +1,8 @@
 package domain;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class Boxes {
 
@@ -10,4 +12,20 @@ public class Boxes {
         this.boxes = boxes;
     }
 
+
+    public List<Box> getBoxes() {
+        return boxes;
+    }
+
+    public Map<Rank, Integer> calculateRanks(){
+        Map<Rank, Integer> result = new EnumMap<>(Rank.class);
+        for(Rank rank : Rank.values()){
+            result.put(rank,0);
+        }
+        for(Box box : boxes){
+            Rank rank = box.calculateRank();
+            result.put(box.calculateRank(),result.getOrDefault(rank,0)+1);
+        }
+        return result;
+    }
 }
