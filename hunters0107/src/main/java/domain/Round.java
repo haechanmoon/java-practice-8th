d104b4883d3dd04496a14d2fbead049011f655b9
haechanmoon <timmymoon@naver.com>
2026-01-07 14:46:52 +0900

    feat(Round) : 자신의 라운드 반환

--- hunters0107/src/main/java/domain/Round.java
+++ hunters0107/src/main/java/domain/Round.java
@@ -1,4 +1,36 @@
 package domain;
 
+import java.util.List;
+import utils.Messages;
+
 public enum Round {
+
+    PITFALL(List.of(0,1,2),-20),
+    MONSTER(List.of(3,4,5),-50),
+    TREASURE(List.of(6,7,8),0),
+    REST(List.of(9),20)
+    ;
+
+    private final List<Integer> random;
+    private final int staminaChange;
+
+
+    Round(List<Integer> random, int staminaChange) {
+        this.random = random;
+        this.staminaChange = staminaChange;
+    }
+
+    public static Round matchRound(int random){
+        for(Round round : Round.values()){
+            if(round.random.contains(random)){
+                return round;
+            }
+        }
+        throw new IllegalArgumentException(Messages.ERROR_NOT_STATE);
+    }
+
+
+    public int getStaminaChange() {
+        return staminaChange;
+    }
 }
--- hunters0107/src/main/java/utils/Messages.java
+++ hunters0107/src/main/java/utils/Messages.java
@@ -2,4 +2,5 @@ package utils;
 
 public class Messages {
     public static final String ERROR_NAME_LENGTH = "이름은 5자 이하여야 합니다.";
+    public static final String ERROR_NOT_STATE = "랜덤값 입력이 옳지 않습니다.";
 }
