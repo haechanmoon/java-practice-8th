18d250a31c494ad7fc1b8c2d6f1b3d8d34745985
haechanmoon <timmymoon@naver.com>
2026-01-07 15:16:19 +0900

    feat(Hunters) : 라운드 별 Hunters의 상태 반환

--- hunters0107/src/main/java/domain/Hunters.java
+++ hunters0107/src/main/java/domain/Hunters.java
@@ -1,4 +1,22 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hunters {
+
+    private final List<Hunter> hunters;
+
+
+    public Hunters(List<Hunter> hunters) {
+        this.hunters = hunters;
+    }
+
+    public List<String> huntersGo(int num){
+        List<String> huntersState = new ArrayList<>();
+        for(Hunter hunter : hunters){
+            huntersState.add(hunter.go(num));
+        }
+        return huntersState;
+    }
 }
