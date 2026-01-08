3e896ddf078f56cb1cdeea5f1e80935fb5c5d10a
haechanmoon <timmymoon@naver.com>
2026-01-08 18:39:22 +0900

    feat : 초기 구조 설정

---
+++ legendary-hunter0108/src/test/java/lotto/ApplicationTest.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApplicationTest extends NsTest {
+       @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
