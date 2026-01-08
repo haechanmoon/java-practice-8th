7958352d7495ec19262ad8ee6677a2dec7b10a25
haechanmoon <timmymoon@naver.com>
2026-01-08 14:07:04 +0900

    feat(WinningNumbers) : 로또번호와 당첨번호의 매칭 개수 반환

--- lotto0108/src/main/java/domain/WinningNumbers.java
+++ lotto0108/src/main/java/domain/WinningNumbers.java
@@ -1,4 +1,16 @@
 package domain;
 
+import java.util.List;
+
 public class WinningNumbers {
+
+    private final List<Integer> winNums;
+
+    public WinningNumbers(List<Integer> winNums) {
+        this.winNums = winNums;
+    }
+
+    public int matchCount(Lotto lotto) {
+        return lotto.matchNumbers(winNums);
+    }
 }
