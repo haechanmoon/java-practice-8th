cfd13bc0fc8ef7fbaa7e359414b5e4dc1dc9fcde
haechanmoon <timmymoon@naver.com>
2026-01-08 13:39:22 +0900

    feat(LottoTest) : 자신의 번호와 당첨번호가 잘 매칭 되는지

--- lotto0108/src/test/java/domain/LottoTest.java
+++ lotto0108/src/test/java/domain/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import utils.Messages;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -28,5 +29,16 @@ class LottoTest {
         assertThatThrownBy(()->new Lotto(List.of(1,2,3,46,45,4)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Messages.ERROR_NUMBER_RANGE);
+    }
+
+    @Test
+    @DisplayName("자신의 번호와 당첨번호가 잘 매칭 되는지")
+    void 로또번호와_당첨번호의_매칭개수(){
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        List<Integer> winningNumbers = List.of(1,2,3,7,8,9);
+
+        int count = lotto.matchNumbers(winningNumbers);
+
+        assertThat(count).isEqualTo(3);
     }
 }
