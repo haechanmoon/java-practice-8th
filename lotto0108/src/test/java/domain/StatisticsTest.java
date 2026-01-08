88ba8e7a47b1890843a565e6446d383518c71e87
haechanmoon <timmymoon@naver.com>
2026-01-08 14:32:19 +0900

    test(StatisticsTest) : 결과가 수익률로 잘 나오는지 -> 테스트 실패

---
+++ lotto0108/src/test/java/domain/StatisticsTest.java
@@ -0,0 +1,23 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StatisticsTest {
+
+    @Test
+    @DisplayName("결과가 수익률로 잘 나오는지")
+    void 결과가_수익률로_잘_나오는지(){
+        int money = 5000;
+        //엇 여기 막혔다.
+
+
+
+
+    }
+
+}
