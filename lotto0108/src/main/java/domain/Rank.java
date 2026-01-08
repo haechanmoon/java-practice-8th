e9589500ca361af7ff3232b059356d56c13278e5
haechanmoon <timmymoon@naver.com>
2026-01-08 13:48:58 +0900

    feat(Rank) : 꽝부터 1등까지의 등수 반환

--- lotto0108/src/main/java/domain/Rank.java
+++ lotto0108/src/main/java/domain/Rank.java
@@ -1,4 +1,40 @@
 package domain;
 
 public enum Rank {
+
+    MISS(0, 0),
+    FIFTH(3, 5000),
+    FOURTH(4, 50000),
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
+    FIRST(6, 2000000000);
+
+    private final int matchCount;
+    private final int prize;
+
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public static Rank matchRank(int matchCount, boolean bonus) {
+
+        if (matchCount < 3) {
+            return MISS;
+        }
+        if (matchCount==5&&bonus){
+            return SECOND;
+        }
+        for(Rank rank : Rank.values()){
+            if(rank.matchCount==matchCount){
+                return rank;
+            }
+        }
+        return MISS;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
 }
